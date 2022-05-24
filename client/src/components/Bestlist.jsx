dd37475d4cbcfb04e8f8d61c5b6abcc4973c3430
M <41164583+ifmcjthenknczny@users.noreply.github.com>
2022-05-24 08:38:28 +0200

    bestlist css width

--- client/src/components/Bestlist.jsx
+++ client/src/components/Bestlist.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import ThemeContext from '../context/theme-context';
+import '../styles/Bestlist.css';
 import ListElement from './ListElement';
 
 export default function Bestlist(props) {
